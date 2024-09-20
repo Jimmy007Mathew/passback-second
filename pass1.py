d72294a5fce19533c03634ddf4a28255445510ef
Jimmy Mathew <jimmymathew765@gmail.com>
2024-09-21 01:49:57 +0530

    Update pass1.py

--- pass1.py
+++ pass1.py
@@ -2,6 +2,12 @@ from pathlib import Path
 import sys
 import math
 
+def design(inline):
+    if len(inline) == 3:
+        symbol, label, opcode = inline[0], inline[1], inline[2]
+        return f"{symbol:<8}{label:<8}{opcode:<9}"
+    elif len(inline) == 2:
+        return f"{inline[0]:<8}{inline[1]:<8}"
 # Ensure paths are provided as command-line arguments
 if len(sys.argv) != 3:
     print("Usage: python pass1.py <input_file_path> <optab_file_path>")
@@ -32,15 +38,15 @@ try:
         if flg==1 and s[1] == 'START' :
             locctr = int(s[2], 16)
             start = locctr
-            file2.write(hex(locctr)[2:].zfill(4) + '\t' + line1)
+            file2.write(hex(locctr)[2:].zfill(4) + '\t' + design(s)+'\n')
 
         for line in file1:
             s = line.split()
             if s[1] == 'END':
-                file2.write(hex(locctr)[2:].zfill(4) + '\t' + line)
+                file2.write(hex(locctr)[2:].zfill(4) + '\t' + design(s)+'\n')
                 break
 
-            file2.write(hex(locctr)[2:].zfill(4) + '\t' + line)
+            file2.write(hex(locctr)[2:].zfill(4) + '\t' + design(s)+'\n')
 
             if s[0] != '**' and s[0] != '-':
                 file3.write(s[0] + '\t' + hex(locctr) + '\n')
